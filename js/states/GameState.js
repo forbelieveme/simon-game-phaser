4d0a72639364815e4f55aa42c031988034693618
Haner Avella <haneravella@MacBook-Pro.local>
2020-08-13 18:52:12 -0500

    Reduce la creacino de elementos a un bucle que lleva la configuracion del json en la carpeta data

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -103,7 +103,7 @@ SimonGame.GameState = {
 			align: 'center',
 		};
 		this.score = this.game.add.text(
-			this.game.world.centerX+145,
+			this.game.world.centerX + 145,
 			42,
 			'',
 			style
@@ -111,79 +111,107 @@ SimonGame.GameState = {
 		this.score.anchor.setTo(0.5);
 		this.score.visible = true;
 
-		this.yellowBtn = this.add.sprite(
-			this.game.world.centerX + this.config.yellowSprite.x,
-			this.game.world.centerY + this.config.yellowSprite.y,
-			'yellowBtn',
-			1
-		);
-		this.yellowBtn.scale.setTo(0.5);
-		this.yellowBtn.anchor.setTo(1, 0);
-		this.yellowBtn.inputEnabled = true;
-		this.yellowBtn.input.pixelPerfectClick = true;
-		this.yellowBtn.events.onInputDown.add(this.animateButton, this);
-		this.yellowAnim = this.yellowBtn.animations.add(
-			'animate',
-			[1, 2, 1],
-			framePerSec,
-			false
-		);
+		// this.yellowBtn = this.add.sprite(
+		// 	this.game.world.centerX + this.config.yellowSprite.x,
+		// 	this.game.world.centerY + this.config.yellowSprite.y,
+		// 	'yellowBtn',
+		// 	1
+		// );
+		// this.yellowBtn.scale.setTo(0.5);
+		// this.yellowBtn.anchor.setTo(1, 0);
+		// this.yellowBtn.inputEnabled = true;
+		// this.yellowBtn.input.pixelPerfectClick = true;
+		// this.yellowBtn.events.onInputDown.add(this.animateButton, this);
+		// this.yellowAnim = this.yellowBtn.animations.add(
+		// 	'animate',
+		// 	[2, 1],
+		// 	framePerSec,
+		// 	false
+		// );
 
-		this.blueBtn = this.add.sprite(
-			this.game.world.centerX + this.config.blueSprite.x,
-			this.game.world.centerY + this.config.blueSprite.y,
-			'blueBtn',
-			1
-		);
-		this.blueBtn.scale.setTo(0.5);
-		this.blueBtn.anchor.setTo(0, 0);
-		this.blueBtn.inputEnabled = true;
-		this.blueBtn.input.pixelPerfectClick = true;
-		this.blueBtn.events.onInputDown.add(this.animateButton, this);
-		this.blueAnim = this.blueBtn.animations.add(
-			'animate',
-			[1, 2, 1],
-			framePerSec,
-			false
-		);
+		// this.blueBtn = this.add.sprite(
+		// 	this.game.world.centerX + this.config.blueSprite.x,
+		// 	this.game.world.centerY + this.config.blueSprite.y,
+		// 	'blueBtn',
+		// 	1
+		// );
+		// this.blueBtn.scale.setTo(0.5);
+		// this.blueBtn.anchor.setTo(0, 0);
+		// this.blueBtn.inputEnabled = true;
+		// this.blueBtn.input.pixelPerfectClick = true;
+		// this.blueBtn.events.onInputDown.add(this.animateButton, this);
+		// this.blueAnim = this.blueBtn.animations.add(
+		// 	'animate',
+		// 	[2, 1],
+		// 	framePerSec,
+		// 	false
+		// );
 
-		this.redBtn = this.add.sprite(
-			this.game.world.centerX + this.config.redSprite.x,
-			this.game.world.centerY + this.config.redSprite.y,
-			'redBtn',
-			1
-		);
-		this.redBtn.scale.setTo(0.5);
-		this.redBtn.anchor.setTo(0, 1);
-		this.redBtn.inputEnabled = true;
-		this.redBtn.input.pixelPerfectClick = true;
-		this.redBtn.events.onInputDown.add(this.animateButton, this);
-		this.redAnim = this.redBtn.animations.add(
-			'animate',
-			[1, 2, 1],
-			framePerSec,
-			false
-		);
+		// this.redBtn = this.add.sprite(
+		// 	this.game.world.centerX + this.config.redSprite.x,
+		// 	this.game.world.centerY + this.config.redSprite.y,
+		// 	'redBtn',
+		// 	1
+		// );
+		// this.redBtn.scale.setTo(0.5);
+		// this.redBtn.anchor.setTo(0, 1);
+		// this.redBtn.inputEnabled = true;
+		// this.redBtn.input.pixelPerfectClick = true;
+		// this.redBtn.events.onInputDown.add(this.animateButton, this);
+		// this.redAnim = this.redBtn.animations.add(
+		// 	'animate',
+		// 	[2, 1],
+		// 	framePerSec,
+		// 	false
+		// );
 
-		this.greenBtn = this.add.sprite(
-			this.game.world.centerX + this.config.greenSprite.x,
-			this.game.world.centerY + this.config.greenSprite.y,
-			'greenBtn',
-			1
-		);
-		this.greenBtn.scale.setTo(0.5);
-		this.greenBtn.anchor.setTo(1, 1);
-		this.greenBtn.inputEnabled = true;
-		this.greenBtn.input.pixelPerfectClick = true;
-		this.greenBtn.events.onInputDown.add(this.animateButton, this);
-		this.greenAnim = this.greenBtn.animations.add(
-			'animate',
-			[1, 2, 1],
-			framePerSec,
-			false
-		);
-		this.anims = [this.yellowAnim, this.blueAnim, this.redAnim, this.greenAnim];
-		this.buttons = [this.yellowBtn, this.blueBtn, this.redBtn, this.greenBtn];
+		// this.greenBtn = this.add.sprite(
+		// 	this.game.world.centerX + this.config.greenSprite.x,
+		// 	this.game.world.centerY + this.config.greenSprite.y,
+		// 	'greenBtn',
+		// 	1
+		// );
+		// this.greenBtn.scale.setTo(0.5);
+		// this.greenBtn.anchor.setTo(1, 1);
+		// this.greenBtn.inputEnabled = true;
+		// this.greenBtn.input.pixelPerfectClick = true;
+		// this.greenBtn.events.onInputDown.add(this.animateButton, this);
+		// this.greenAnim = this.greenBtn.animations.add(
+		// 	'animate',
+		// 	[2, 1],
+		// 	framePerSec,
+		// 	false
+		// );
+
+		this.buttons = this.game.add.group();
+		var button;
+		this.anims = [];
+		this.buttonArr = []
+
+		this.config.buttonData.forEach((element) => {
+			button = this.buttons.create(
+				this.game.world.centerX + element.x,
+				this.game.world.centerY + element.y,
+				element.key,
+				1
+			);
+
+			button.customParams = {
+				sound: this.game.add.audio(element.audio),
+			};
+			button.anchor.setTo(element.anchorX, element.anchorY);
+			button.scale.setTo(0.5);
+
+			button.inputEnabled = true;
+			button.input.pixelPerfectClick = true;
+			button.events.onInputDown.add(this.animateButton, this);
+
+			this.anims.push(button.animations.add('animate', [2, 1], framePerSec, false));
+			this.buttonArr.push(button);
+		}, this);
+
+		// this.anims = [this.yellowAnim, this.blueAnim, this.redAnim, this.greenAnim];
+		// this.buttons = [this.yellowBtn, this.blueBtn, this.redBtn, this.greenBtn];
 
 		this.uiBlocked = false;
 		this.playerTurn = false;
@@ -211,8 +239,10 @@ SimonGame.GameState = {
 		}
 	},
 	createSequence: function () {
+		console.log(`buttons: `, this.buttonArr);
+		
 		this.sequenceGame.push(
-			this.buttons[this.game.rnd.integerInRange(0, 3)].key
+			this.buttonArr[this.game.rnd.integerInRange(0, 3)].key
 		);
 		this.playSequence(0)
 	},
@@ -238,16 +268,16 @@ SimonGame.GameState = {
 	auxiliar: function (i) {
 		switch (this.sequenceGame[i]) {
 			case 'yellowBtn':
-				this.animateButton(this.buttons[0]);
+				this.animateButton(this.buttonArr[0]);
 				break;
 			case 'blueBtn':
-				this.animateButton(this.buttons[1]);
+				this.animateButton(this.buttonArr[1]);
 				break;
 			case 'redBtn':
-				this.animateButton(this.buttons[2]);
+				this.animateButton(this.buttonArr[2]);
 				break;
 			case 'greenBtn':
-				this.animateButton(this.buttons[3]);
+				this.animateButton(this.buttonArr[3]);
 				break;
 		}
 		this.playSequence(i + 1)
