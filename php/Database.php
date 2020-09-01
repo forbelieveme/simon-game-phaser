451e402ef408135603b775f8df4f302eb473b8d7
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-09-01 11:18:01 -0500

    Cambia de PDO a MYSQLi la conexion de la base datos

--- php/Database.php
+++ php/Database.php
@@ -1,30 +1,30 @@
 <?php
 ini_set('html_errors', false);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 class Database
 {
-
-    /* Properties */
-    private $conn;
-    private $dsn = 'mysql:dbname=meccanim_simon;host=127.0.0.1';
-    private $user = 'root';
+    private $host = 'localhost';
+    private $db_name = 'meccanim_simon';
+    private $username = 'root';
     private $password = '';
+    private $conn;
 
-    /* Creates database connection */
-    public function __construct()
+    public function connect()
     {
-        try {
-            $this->conn = new PDO($this->dsn, $this->user, $this->password);
-        } catch (PDOException $e) {
-            print "Error!: " . $e->getMessage() . "";
-            die();
-        }
-        return $this->conn;
-    }
-
-    public function __destruct()
-    {
-        // Disconnect from DB
         $this->conn = null;
-        echo 'Successfully disconnected from the database!';
+
+        // El '@' suprime el error que arroja la creación de la conexión
+        // $this->conn = @new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        try {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        } catch (Exception $e) {
+            die(json_encode([
+                'error' => mysqli_connect_error(),
+                'code' => mysqli_connect_errno(),
+                'excepcion' => $e->getMessage()
+            ]));
+        }
+
+        return $this->conn;
     }
 }
