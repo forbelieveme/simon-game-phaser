4b01ab1578847c265ced09923ae2f773a90b68a6
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-09-03 02:05:29 -0500

    Inserta la funcion que realiza la peticion ajax de validacio de usuario

--- js/main.js
+++ js/main.js
@@ -1,47 +1,20 @@
 var SimonGame = SimonGame || {};
 var juego;
+
 function validar_ingreso() {
     var cedula, infoFormIngresoUsuario;
     cedula = $('#cedula').val();
-    //console.log('success', `${cedula}`);
 
     infoFormIngresoUsuario = $('#info_form_ingreso_usuario');
 
     if (cedula == '') {
-        // MostrarInfoForm(infoFormIngresoUsuario, 'Complete el campo Cédula.');
+        mostrarInfoFormulario(infoFormIngresoUsuario, 'Complete el campo Cédula.');
     } else if (isNaN(cedula) || cedula.length > 11) {
-        // MostrarInfoForm(infoFormIngresoUsuario, 'Cédula no válida.');
+        mostrarInfoFormulario(infoFormIngresoUsuario, 'Cédula no válida.');
     } else {
         $('#btn_form_ingreso_usuario').prop('disabled', true);
-
-        // $.ajax({
-        //     url: 'assets/php/peticiones.php',
-        //     data: {
-        //         query: 'validacionIngresoUsusario',
-        //         cedula,
-        //     },
-        //     type: 'POST',
-        //     success: function (datos) {
-                // switch (datos['caso']) {
-                //     case 0:
-                //         console.log(`Error : ${datos.mensaje}`);
-                //         MostrarInfoForm(infoFormIngresoUsuario, datos.mensaje);
-                //         break;
-                //     case 1:
-                //         //console.log(`Exito : ${JSON.stringify(datos)}`);
-                //         juego.global = datos;
-                //         // console.log(`${validarCantidadJuegos(datos.id)}`);
-
-                        iniciarJuego();
-                //         break;
-                //     default:
-                //         console.log('success', datos);
-
-                //         break;
-                // }
-                $('#btn_form_ingreso_usuario').prop('disabled', false);
-            // },
-        // });
+        peticionValidacion(cedula, infoFormIngresoUsuario);
+        $('#btn_form_ingreso_usuario').prop('disabled', false);
     }
 }
 
@@ -55,4 +28,40 @@ function iniciarJuego() {
         SimonGame.game.state.add('PreloadState', SimonGame.PreloadState);
         SimonGame.game.state.start('BootState');
     }
+}
+
+function mostrarInfoFormulario(infoForm, textoInfo) {
+    infoForm.html(textoInfo);
+    setTimeout(function () {
+        infoForm.html('');
+    }, 1800);
+}
+
+function peticionValidacion(cedula, infoFormIngresoUsuario) {
+    $.ajax({
+        url: 'php/Usuario.php',
+        data: {
+            query: 'validacionIngresoUsusario',
+            cedula,
+        },
+        type: 'POST',
+        success: function (datos) {
+            switch (datos['caso']) {
+                case 0:
+                    console.log(`Error : ${datos.mensaje}`);
+                    mostrarInfoFormulario(infoFormIngresoUsuario, datos.mensaje);
+                    break;
+                case 1:
+                    iniciarJuego();
+                    break;
+                default:
+                    console.log('success', datos);
+
+                    break;
+                }
+        },
+        error: function (error) {
+            console.log(`error`, error);
+        }
+    });
 }
