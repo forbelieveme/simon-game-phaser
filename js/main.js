c9a30f0eb18e18b7a2c7d0e610710432667f652e
Haner Avella <haneravella@MacBook-Pro-2.local>
2020-09-04 22:50:56 -0500

    Corrige la variable de SimonGame.HomeState

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function iniciarJuego() {
         SimonGame.game = new Phaser.Game(960, 506, Phaser.AUTO, 'game_block');
 
         SimonGame.game.state.add('GameState', SimonGame.GameState);
-        SimonGame.game.state.add('HomeState', SimonGame.GameState);
+        SimonGame.game.state.add('HomeState', SimonGame.HomeState);
         SimonGame.game.state.add('BootState', SimonGame.BootState);
         SimonGame.game.state.add('PreloadState', SimonGame.PreloadState);
         SimonGame.game.state.start('BootState');
